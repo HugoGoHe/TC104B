de673b785e276ff319df92fd316940aaae6a7529
HugoGoHe <A01640856@itesm.mx>
2022-10-06 13:49:49 +0000

    Practica 3

---
+++ tomay.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+
+
+int main(){
+    char c;
+    char may;
+    int n;
+    do{
+        n = read(STDIN_FILENO, &c,1);
+        if (c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u'){
+            may = '1';
+            write(STDOUT_FILENO, &may,1);
+        }
+
+    }while(n!=0);
+    return 0;
+}
